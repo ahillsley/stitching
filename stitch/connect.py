259a3412c8425980e98e403b6d84229d393d99a1
Alexander Hillsley <alexander.hillsley@czbiohub.org>
2025-03-10 16:38:11 -0700

    register

--- stitch/connect.py
+++ stitch/connect.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def parse_positions(fovs: list)->np.array:
+
+def parse_positions(fovs: list) -> np.array:
     """
     read FOV names and parse into X/Y coordinates assuming that each FOV is named XXXYYY convention
     """
@@ -10,15 +11,15 @@ def parse_positions(fovs: list)->np.array:
         x_cord = int(p[:3])
         y_cord = int(p[3:])
         fov_positions.append((x_cord, y_cord))
-        
+
     return np.asarray(fov_positions)
 
 
-def connectivity(points: np.array)->np.array:
+def connectivity(points: np.array) -> np.array:
     """
     provided a list of points, get the connectivity graph between neighboring points
     """
-    point_set = set(map(tuple,points))
+    point_set = set(map(tuple, points))
     edges = set()
     directions = np.array([(0, 1), (1, 0)])
     for x, y in points:
--- stitch/generate.py
+++ stitch/generate.py
@@ -1,45 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def example_tiles(
-        img: np.array,
-        n: int,
-        overlap: float,
-)->list:
+    img: np.array,
+    n: int,
+    overlap: float,
+) -> list:
     tiles = []
     width, height = img.shape[:2]
-    tile_height = height//n
-    tile_width = width//n
+    tile_height = height // n
+    tile_width = width // n
     i_stride = tile_height - overlap
     j_stride = tile_width - overlap
 
     for i in range(n):
         for j in range(n):
-            i_shift, j_shift = np.random.randint(-0.2*overlap, 0.2*overlap, 2)
+            i_shift, j_shift = np.random.randint(-0.2 * overlap, 0.2 * overlap, 2)
             i_start = np.clip(i * i_stride + i_shift, a_min=0, a_max=height)
             i_end = np.clip(i_start + tile_height, a_min=0, a_max=height)
             j_start = np.clip(j * j_stride + j_shift, a_min=0, a_max=width)
             j_end = np.clip(j_start + tile_width, a_min=0, a_max=width)
 
-            print(f'{i_start}, {i_end}, {j_start}, {j_end}')
+            print(f"{i_start}, {i_end}, {j_start}, {j_end}")
 
-            tiles.append(
-                img[
-                    i_start:i_end,
-                    j_start:j_end
-                ]
-            )
+            tiles.append(img[i_start:i_end, j_start:j_end])
 
     return tiles
 
-def circle_points(center: tuple, radius: int)->np.array:
+
+def circle_points(center: tuple, radius: int) -> np.array:
     x_c, y_c = center
     points = []
 
     # Iterate over a bounding box of the circle
     for x in range(x_c - radius, x_c + radius + 1):
         for y in range(y_c - radius, y_c + radius + 1):
-            if (x - x_c)**2 + (y - y_c)**2 <= radius**2:
+            if (x - x_c) ** 2 + (y - y_c) ** 2 <= radius**2:
                 points.append((x, y))
 
     return np.array(points)
