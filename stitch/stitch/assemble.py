127fcafd66d491302c5bf71f3f5cc53df7c7fc85
Alexander Hillsley <alexander.hillsley@czbiohub.org>
2025-03-28 16:21:06 -0700

    add functionality to estimate stitching parameters

--- stitch/stitch/assemble.py
+++ stitch/stitch/assemble.py
@@ -1,12 +1,16 @@
 from iohub.ngff import open_ome_zarr
 from tqdm import tqdm
-from stitch.stitch.tile import augment_tile
+from stitch.stitch.tile import augment_tile, pairwise_shifts, optimal_positions
+from stitch.connect import read_shifts_biahub
+from collections import defaultdict
+from iohub.ngff import TransformationMeta
+from typing import Any, Callable, Dict, Literal, Optional, Tuple, Type, Union
+import itertools
+from numpy.typing import ArrayLike
+from itertools import product
+import yaml
+from pathlib import Path
 
-# try:
-#     import cupy as xp
-#     from cupyx.scipy import ndimage as cundi
-
-# except (ModuleNotFoundError, ImportError):
 import numpy as xp
 from scipy import ndimage as cundi
 
@@ -29,6 +33,7 @@ def assemble(
     flipud: bool = False,
     fliplr: bool = False,
     rot90: int = 0,
+    divide_tile_size: Optional[Tuple[int, int]] = (4000, 4000),
 ):
     """Assemble the stitched image give the total shift of all tiles
     - Assume that we have paired image / total shift to be applied
@@ -38,42 +43,26 @@ def assemble(
         - fov_store_path: path to the zarr file with the tiles
     """
 
-    # TODO: Add option for flips
-
     final_shape_xy = get_output_shape(shifts, tile_size)
-
+    print(f"final shape  xy is: {final_shape_xy}")
     fov_store = open_ome_zarr(fov_store_path)
     pos_1 = next(fov_store.positions())[0]
 
     tile_shape = fov_store[pos_1].data.shape
     final_shape = tile_shape[:3] + final_shape_xy
     print(final_shape)
-    output_image = xp.zeros(final_shape, dtype=xp.float32)  # check dtype
+    output_image = xp.zeros(final_shape, dtype=xp.float16)  # check dtype
     divisor = xp.zeros(final_shape, dtype=xp.uint8)
 
-    count = 0
     for tile_name, shift in tqdm(shifts.items()):
-        if count == 196:
-            break
 
         tile = fov_store[tile_name].data  # 5D array OME (T, C, Z, Y, X)
         tile = augment_tile(xp.asarray(tile), flipud, fliplr, rot90)
-
-        # ignore sub-pixel shifts (which biahub does to by order=0 interpolation)
+        # ignore sub-pixel shifts (which biahub does too by order=0 interpolation)
         shift_array = xp.asarray(shift).astype(xp.uint16)
-        # shift_remain = xp.sum(shift_array - shift_array.astype(int))
-        # Check if the shift is exact or if we need to interpolate
-        # if shift_remain == 0.0:
-        #     print(shift_remain)
-        #     print(f'shift was even: {shift_array}')
-        # else:
-        #     print(f'shift was not even: {shift_array}')
-        #     padded_tile = xp.pad(tile, pad_width=((0, 1), (0, 1)))
+        # Future: add rotation / interpolation by first padding, then placing padded block into
+        # final output
 
-        # pad the image
-        # shift the image
-        # crop the image
-        # Index the image into the output image
         output_image[
             :,
             :,
@@ -81,6 +70,7 @@ def assemble(
             shift_array[0] : shift_array[0] + tile_size[0],
             shift_array[1] : shift_array[1] + tile_size[1],
         ] += tile
+
         divisor[
             :,
             :,
@@ -88,11 +78,197 @@ def assemble(
             shift_array[0] : shift_array[0] + tile_size[0],
             shift_array[1] : shift_array[1] + tile_size[1],
         ] += 1
-        # index an image of 1s into the "divisor" output image
-        count += 1
 
-    # divide the images
-    stitched = output_image / divisor
-    stitched = xp.nan_to_num(stitched)
+    stitched = xp.zeros_like(output_image, dtype=xp.float16)
+
+    def _divide(a, b):
+        return xp.nan_to_num(a / b)
+
+    out = divide_tile(
+        output_image,
+        divisor,
+        func=_divide,
+        out_array=stitched,
+        tile=divide_tile_size,  # need to increase
+    )
+    del out  # free memory
 
     return stitched
+
+
+def stitch(
+    config_path: str,
+    input_store_path: str,
+    output_store_path: str,
+    flipud: bool = False,
+    fliplr: bool = False,
+    rot90: int = 0,
+    **kwargs,
+):
+    """Mimic of biahub stitch function"""
+
+    # get the shifts and split into a list of lists per well
+    all_shifts = read_shifts_biahub(config_path)
+
+    def get_group(key):
+        return key.split("/")[1]
+
+    grouped_shifts = defaultdict(dict)
+    for key, value in all_shifts.items():
+        group = get_group(key)
+        grouped_shifts[group][key] = value
+
+    input_store = open_ome_zarr(input_store_path)
+    channel_names = input_store.channel_names
+    temp_pos = next(input_store.positions())[0]
+    tile_shape = input_store[temp_pos].data.shape
+    chunks_size = input_store[temp_pos].data.chunks
+    scale = input_store[temp_pos].scale
+
+    # initialize output zarr store
+    output_store = open_ome_zarr(
+        output_store_path, layout="hcs", mode="w-", channel_names=channel_names
+    )
+    print("output store created")
+    # call assemble for each well
+    for g in grouped_shifts.keys():
+        shifts = grouped_shifts[g]
+        print("entering assemble")
+        output = assemble(
+            shifts=shifts,
+            tile_size=tile_shape[-2:],
+            fov_store_path=input_store_path,
+            flipud=flipud,
+            fliplr=fliplr,
+            rot90=rot90,
+        )
+        print("completed output")
+        stitched_pos = output_store.create_position("A", g, "0")
+        print("created position")
+        stitched_pos.create_image(
+            "0",
+            data=output,
+            chunks=chunks_size,
+            transform=[TransformationMeta(type="scale", scale=scale)],
+        )
+        del output  # free memory
+        del stitched_pos  # free memory
+        print("-" * 30)
+        print("finished a well")
+        print("-" * 30)
+
+    return
+
+
+def estimate_stitch(
+    input_store_path: str,
+    output_config_path: Path,
+    flipud: bool,
+    fliplr: bool,
+    rot90: int,
+):
+    """Mimic of Biahub estimate stitch function"""
+
+    store = open_ome_zarr(input_store_path)
+    position_list = [a[0] for a in store.positions()]
+
+    grouped_positions = defaultdict(list)
+    for a in position_list:
+        group = a[:3]
+        grouped_positions[group].append(a)
+
+    running_opt_shift_dict = {}
+    running_confidence_dict = {}
+    for g in grouped_positions.keys():
+        well_positions = grouped_positions[g]
+        tile_lut = {t[4:]: i for i, t in enumerate(well_positions)}
+
+        edge_list, confidence_dict = pairwise_shifts(
+            well_positions,
+            input_store_path,
+            well=g,
+            flipud=flipud,
+            fliplr=fliplr,
+            rot90=rot90,
+        )
+
+        opt_shift_dict = optimal_positions(edge_list, tile_lut, g)
+
+        running_opt_shift_dict = running_opt_shift_dict | opt_shift_dict
+        running_confidence_dict[g] = confidence_dict
+
+    to_write = {
+        "total_translation": running_opt_shift_dict,
+        "confidence": running_confidence_dict,
+    }
+    with open(output_config_path, "w") as f:
+        yaml.dump(to_write, f)
+
+    return opt_shift_dict
+
+
+def array_apply(
+    *in_arrays: ArrayLike,
+    func: Callable,
+    out_array: ArrayLike,
+    axis: Union[Tuple[int], int] = 0,
+    **kwargs,
+) -> ArrayLike:
+    """Apply a function over a given dimension of an array.
+
+    adapted from Ultrack.apply_array
+    """
+    name = func.__name__ if hasattr(func, "__name__") else type(func).__name__
+
+    try:
+        in_shape = [arr.shape for arr in in_arrays]
+        xp.broadcast_shapes(out_array.shape, *in_shape)
+    except ValueError as e:
+        print(
+            f"Warning: if you are not using multichannel operations, "
+            f"this can be an error. {e}."
+        )
+
+    if isinstance(axis, int):
+        axis = (axis,)
+
+    stub_slicing = [slice(None) for _ in range(out_array.ndim)]
+    multi_indices = list(itertools.product(*[range(out_array.shape[i]) for i in axis]))
+    for indices in tqdm(multi_indices, f"Applying {name} ..."):
+        for a, i in zip(axis, indices):
+            stub_slicing[a] = i
+        indexing = tuple(stub_slicing)
+
+        func_result = func(*[a[indexing] for a in in_arrays], **kwargs)
+        output_shape = out_array[indexing].shape
+        out_array[indexing] = xp.broadcast_to(func_result, output_shape)
+
+    return out_array
+
+
+def divide_tile(
+    *in_arrays: ArrayLike,
+    func: Callable,
+    out_array: ArrayLike,
+    tile: tuple,
+    overlap: tuple = (0, 0),
+):
+
+    final_shape = out_array.shape[-2:]
+
+    tiling_start = list(
+        product(
+            *[
+                range(o, size + 2 * o, t + o)  # t + o step, because of blending map
+                for size, t, o in zip(final_shape, tile, overlap)
+            ]
+        )
+    )
+    for start_indices in tqdm(tiling_start, "Applying function to tiles"):
+        slicing = (...,) + tuple(
+            slice(start - o, start + t + o)
+            for start, t, o in zip(start_indices, tile, overlap)
+        )
+        out_array[slicing] = func(*[a[slicing] for a in in_arrays])
+
+    return out_array
--- stitch/stitch/tile.py
+++ stitch/stitch/tile.py
@@ -1,9 +1,19 @@
 import numpy as np
-from stitch.stitch.align import offset
 from stitch import connect
 from collections import OrderedDict
 from iohub import open_ome_zarr
 import dask.array as da
+from typing import List, Dict
+from tqdm import tqdm
+import scipy
+
+from dexp.processing.registration.model.translation_registration_model import (
+    TranslationRegistrationModel,
+)
+from dexp.processing.registration import translation_nd as dexp_reg
+from dexp.processing.utils.linear_solver import linsolve
+from stitch.connect import parse_positions, pos_to_name
+from stitch.stitch.graph import connectivity, hilbert_over_points
 
 
 class LimitedSizeDict(OrderedDict):
@@ -30,7 +40,12 @@ class Edge:
         self.tile_cache = tile_cache
         self.tile_a = connect.pos_to_name(tile_a_key)
         self.tile_b = connect.pos_to_name(tile_b_key)
-        self.offset = self.get_offset()
+        self.ux = int(self.tile_a[:3])
+        self.uy = int(self.tile_a[3:])
+        self.vx = int(self.tile_b[:3])
+        self.vy = int(self.tile_b[3:])
+        self.relation = (self.ux - self.vx, self.uy - self.vy)
+        self.model = self.get_offset()
 
     def get_offset(self):
         """
@@ -38,7 +53,7 @@ class Edge:
         """
         tile_a = self.tile_cache[self.tile_a]
         tile_b = self.tile_cache[self.tile_b]
-        return offset(tile_a, tile_b)
+        return offset(tile_a, tile_b, self.relation, overlap=150)
 
 
 class TileCache:
@@ -52,10 +67,13 @@ class TileCache:
     - get_item
     """
 
-    def __init__(self, store_path, well):
-        self.cache = LimitedSizeDict(max_size=10)
+    def __init__(self, store_path, well, flipud, fliplr, rot90):
+        self.cache = LimitedSizeDict(max_size=20)
         self.store = open_ome_zarr(store_path)
         self.well = well
+        self.flipud = flipud
+        self.fliplr = fliplr
+        self.rot90 = rot90
 
     def add(self, obj):
         """add an object to the cache"""
@@ -78,12 +96,17 @@ class TileCache:
 
     def load_tile(self, key):
         """load a tile and add it to the cache"""
-        print("loading tile")
         da_tile = da.from_array(self.store[f"{self.well}/{key}"].data)
 
+        aug_tile = augment_tile(
+            da_tile[0, 0, 0, :, :].compute(),
+            flipud=self.flipud,
+            fliplr=self.fliplr,
+            rot90=self.rot90,
+        )
         # hardcoded for now to only take first slice and time-point
-        self.cache[key] = da_tile[0, 0, 0, :, :]
-        return da_tile[0, 0, 0, :, :]
+        self.cache[key] = aug_tile
+        return aug_tile
 
 
 def augment_tile(tile: np.ndarray, flipud: bool, fliplr: bool, rot90: int) -> np.array:
@@ -93,5 +116,146 @@ def augment_tile(tile: np.ndarray, flipud: bool, fliplr: bool, rot90: int) -> np
     if fliplr:
         tile = np.flip(tile, axis=-1)
     if rot90:
-        tile = np.rot90(tile, k=rot90)
+        tile = np.rot90(tile, k=rot90, axes=(-2, -1))
     return tile
+
+
+def offset(
+    image_a: np.array, image_b: np.array, relation: tuple, overlap: int
+) -> TranslationRegistrationModel:
+    """
+    overlap: estimate of the # pixels seen in both images
+    """
+    shape = image_a.shape
+    # TODO: have this load only a small fraction of the entire image
+    if relation[0] == -1:
+        # tile_b is to the right of tile_a
+        roi_a = image_a[:, -overlap:]
+        roi_b = image_b[:, :overlap]
+        corr_x = shape[-2] - overlap
+        corr_y = 0
+
+    if relation[1] == -1:
+        # tile_b is below tile_a
+        roi_a = image_a[-overlap:, :]
+        roi_b = image_b[:overlap, :]
+        corr_x = 0
+        corr_y = shape[-1] - overlap
+
+    # Currently edges only exist to the right and below
+    # This is by desgin when we construct the graph
+    # however leave this here for future use
+    # Caution: not tested yet
+    # if relation[0] == 1:
+    # # tile_b is to the left of tile_a
+    # roi_a = image_a[:, overlap:]
+    # roi_b = image_b[:, -overlap]
+    # corr_x = overlap - shape[2]
+    # corr_y = 0
+    # sign_flipper = np.asarray([1, 1])
+    # if relation[1] == 1:
+    #     # tile_b is above tile_a
+    #     roi_a = image_a[-overlap:, :]
+    #     roi_b = image_b[:overlap, :]
+    #     corr_x = 0
+    #     corr_y =  overlap - shape[1]
+    #     sign_flipper = np.asarray([1, 1])s
+
+    model = dexp_reg.register_translation_nd(roi_a, roi_b)
+    # print(f"model shift vector is: {model.shift_vector}")
+    model.shift_vector += np.array(
+        [corr_y, corr_x]
+    )  # add the correction to the shift vector
+
+    return model
+
+
+def pairwise_shifts(
+    positions: List, store_path: str, well: str, flipud: bool, fliplr: bool, rot90: bool
+) -> List:
+    """ """
+    # get neighboring tiles
+    grid_positions = parse_positions(positions)
+    hilbert_order = hilbert_over_points(grid_positions)
+    edges_hilbert = connectivity(hilbert_order)
+
+    tile_cache = TileCache(
+        store_path=store_path,
+        well=well,
+        flipud=flipud,
+        fliplr=fliplr,
+        rot90=rot90,
+    )
+
+    edge_list = []
+    edge_list = []
+    confidence_dict = {}
+    for key, pos in tqdm(edges_hilbert.items()):
+        edge_model = Edge(pos[0], pos[1], tile_cache)
+        edge_list.append(edge_model)
+
+        # positions need to be not np.types to save to yaml
+        pos_a_nice = list(int(x) for x in pos[0])
+        pos_b_nice = list(int(x) for x in pos[1])
+        confidence_dict[key] = [
+            pos_a_nice,
+            pos_b_nice,
+            float(edge_model.model.confidence),
+        ]
+
+    return edge_list, confidence_dict
+
+
+def optimal_positions(edge_list: List, tile_lut: Dict, well: str) -> Dict:
+    """ """
+    y_i = np.zeros(len(edge_list) + 1, dtype=np.float32)
+    y_j = np.zeros(len(edge_list) + 1, dtype=np.float32)
+
+    x_guess = np.asarray([int(a[:3]) * 2048 for a in tile_lut.keys()])
+
+    a = scipy.sparse.lil_matrix((len(tile_lut), len(edge_list) + 1), dtype=np.float32)
+
+    for c, e in enumerate(edge_list):
+        a[[tile_lut[e.tile_a], tile_lut[e.tile_b]], c] = [-1, 1]
+        y_i[c] = e.model.shift_vector[0]
+        y_j[c] = e.model.shift_vector[1]
+
+    y_i[-1] = 0
+    y_j[-1] = 0
+    a[0, -1] = 1
+
+    a = a.T.tocsr()
+    tolerance = 1e-6
+    order_error = 1
+    order_reg = 1
+    alpha_reg = 0
+
+    opt_i = linsolve(
+        a,
+        y_i,
+        tolerance=tolerance,
+        order_error=order_error,
+        order_reg=order_reg,
+        alpha_reg=alpha_reg,
+        x0=x_guess,
+    )
+    opt_j = linsolve(
+        a,
+        y_j,
+        tolerance=tolerance,
+        order_error=order_error,
+        order_reg=order_reg,
+        alpha_reg=alpha_reg,
+        x0=x_guess,
+    )
+    opt_shifts = np.vstack((opt_i, opt_j)).T
+
+    opt_shifts_zeroed = opt_shifts - np.min(opt_shifts, axis=0)
+
+    # needs to be a list of python types for exporting to yaml
+    opt_shifts_dict = {
+        f"{well}/{a}": [int(a) for a in opt_shifts_zeroed[i]]
+        for i, a in enumerate(tile_lut.keys())
+    }
+
+    return opt_shifts_dict
